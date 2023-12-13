daa8a400eae8109168783d65987b4d3a0482c86a
kozue-y <143789618+kozue-y@users.noreply.github.com>
2023-12-13 23:20:03 +0900

    Add files via upload

---
+++ javascript-basic-kadai/kadai_020/event.js
@@ -0,0 +1,6 @@
+const kadaibtn=document.getElementById('btn');
+const kadaitext=document.getElementById('text');
+
+kadaibtn.addEventListener ('click' , () => {
+  kadaitext.textContent ='ボタンをクリックしました'
+} )
